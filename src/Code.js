a6bb429ad78f17598ac61bd1dd0699896bed1327
Denis <denis@karbach.dev>
2026-02-06 20:06:47 +0100

    refactor: Umstrukturierung der Dateiorganisation, Verschiebung von Formeldateien in das `formulas/` Verzeichnis und Hinzufügung von Lizenz- und Dokumentationsdateien.

--- src/Code.js
+++ src/Code.js
@@ -1,3 +1,21 @@
+/**
+ * Zuschuss-System V8
+ * Copyright (C) 2026 Denis Karbach
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 // onOpen wurde entfernt, um Ladezeiten zu optimieren.
 // Die Sidebar wird nun ausschließlich über den Button im SETUP-Blatt gestartet.
 
